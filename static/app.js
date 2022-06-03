4114af8d8cd76a7acd6448525044dfdd932f9d04
Bruno Divino <brunodivino02@gmail.com>
2022-06-03 11:33:41 -0300

    Resultado após vídeo 4.1

---
+++ static/app.js
@@ -0,0 +1,15 @@
+$('form input[type="file"]').change(event => {
+  let arquivos = event.target.files;
+  if (arquivos.length === 0) {
+    console.log('sem imagem pra mostrar')
+  } else {
+      if(arquivos[0].type == 'image/jpeg') {
+        $('img').remove();
+        let imagem = $('<img class="img-fluid">');
+        imagem.attr('src', window.URL.createObjectURL(arquivos[0]));
+        $('figure').prepend(imagem);
+      } else {
+        alert('Formato não suportado')
+      }
+  }
+});
