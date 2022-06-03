c904df70047b4eed79f0516ca2484304e5e1f057
Bruno Divino <brunodivino02@gmail.com>
2022-06-03 11:30:01 -0300

    Resultado após vídeo 3.3

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, session, flash, url_for
+from flask import render_template, request, redirect, session, flash, url_for, send_from_directory
 from jogoteca import app, db
 from models import Jogos, Usuarios
 
@@ -89,3 +89,7 @@ def logout():
     session['usuario_logado'] = None
     flash('Logout efetuado com sucesso!')
     return redirect(url_for('index'))
+
+@app.route('/uploads/<nome_arquivo>')
+def imagem(nome_arquivo):
+    return send_from_directory('uploads', nome_arquivo)
