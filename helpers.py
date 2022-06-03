dca03d2c6c42417a38108ff1bfde68d5e55199cf
Bruno Divino <brunodivino02@gmail.com>
2022-06-03 11:42:29 -0300

    Resultado após vídeo 5.2

--- helpers.py
+++ helpers.py
@@ -1,5 +1,13 @@
 import os
 from jogoteca import app
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, validators
+
+class FormularioJogo(FlaskForm):
+    nome = StringField('Nome do Jogo', [validators.DataRequired(), validators.Length(min=1, max=50)])
+    categoria = StringField('Categoria', [validators.DataRequired(), validators.Length(min=1, max=40)])
+    console = StringField('Console', [validators.DataRequired(), validators.Length(min=1, max=20)])
+    salvar = SubmitField('Salvar')
 
 def recupera_imagem(id):
     for nome_arquivo in os.listdir(app.config['UPLOAD_PATH']):
