7e729b1893597e387c1d014cfa7cb8a1c543985c
Bruno Divino <brunodivino02@gmail.com>
2022-06-03 10:52:46 -0300

    Resultado após vídeo 1.5

--- jogoteca.py
+++ jogoteca.py
@@ -40,6 +40,24 @@ app.config['SQLALCHEMY_DATABASE_URI'] = \
 
 db = SQLAlchemy(app)
 
+class Jogos(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(50), nullable=False)
+    categoria = db.Column(db.String(40), nullable=False)
+    console = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+
+class Usuarios(db.Model):
+    nickname = db.Column(db.String(8), primary_key=True)
+    nome = db.Column(db.String(20), nullable=False)
+    senha = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
 @app.route('/')
 def index():
     return render_template('lista.html', titulo='Jogos', jogos=lista)
